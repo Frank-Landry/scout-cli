7016840d28f53af00507001d1795105ff1ca134a
Frank-Landry <franklandry85@gmail.com>
2021-10-13 23:20:34 -0400

    WIP: added progress bar and requests out to addresses at policy urls, but it seems to stop at 17/18 for some reason

--- app/Commands/CheckHeader.php
+++ app/Commands/CheckHeader.php
@@ -5,7 +5,7 @@ namespace App\Commands;
 use Illuminate\Console\Scheduling\Schedule;
 use LaravelZero\Framework\Commands\Command;
 use App\Services\GetterService;
-
+use Illuminate\Support\Facades\Storage;
 class CheckHeader extends Command
 {
     /**
@@ -23,16 +23,57 @@ class CheckHeader extends Command
     protected $description = 'Use this command to check the bounty lists for entries who have a header that matches what you provide.';
 
     /**
+     * Check the entries in the results.json file for the header that you specified.
+     *
+     * @param GetterService $gserve
+     * @param string $file_path
+     * @param string $header
+     * @return void
+     */
+    private function checkEntries(GetterService $gserve, string $file_path, string $header) : void
+    {
+        Storage::put('filtered_results.json', '[');
+        $json = Storage::get($file_path);
+        $entries = json_decode($json);
+        $bar = $this->output->createProgressBar(21);
+        $bar->start();
+        $i = 0;
+        foreach($entries as $key => $value)
+        {
+            $i++;
+            $response_headers = $gserve->getHeaders($value->policy_url);
+            $match = $gserve->verifyHeaderExists($header, $response_headers);
+            if($match)
+            {
+                Storage::append('filtered_results.json', json_encode($value, JSON_PRETTY_PRINT));
+                Storage::append('filtered_results.json', ',');
+            }
+            $bar->advance();
+            if($i >20)
+            {
+                break;
+            }
+        }
+        $bar->finish();
+    }
+
+    /**
      * Execute the console command.
      *
      * @return mixed
      */
-    public function handle(GetterService $gserve)
+    public function handle(GetterService $gserve) : void
     {
-        $header = $this->ask('What header are you checking for?', 'content-type');
+        $this->info('Checking for list file.');
+        if(!file_exists('storage/results.json'))
+        {
+            $this->error('A results file in json format must exist first. Did you forget to run fetch:list first?');
+            exit(0);
+        }
+        $this->comment('File exists');
+        $header = $this->ask('What header are you checking for?', 'Content-Type');
         $this->info('checking');
-        // todo pick out target url from results.json file
-        $response_headers = $gserve->getHeaders($target_url, $header);
+        $this->checkEntries($gserve, 'results.json', $header);
         $this->info('done');
     }
 
--- app/Services/GetterService.php
+++ app/Services/GetterService.php
@@ -4,8 +4,14 @@ namespace App\Services;
 use Illuminate\Support\Facades\Http;
 
 class GetterService {
-    public function getHeaders($target_url, $headers)
+    public function getHeaders(string $target_url) : array
     {
-        // todo
+        $response = Http::withOptions(array('verify' => false))->get($target_url);
+        return $response->headers();
+    }
+
+    public function verifyHeaderExists(string $header, array $headers) : bool
+    {
+        return array_key_exists($header, $headers);
     }
 }
